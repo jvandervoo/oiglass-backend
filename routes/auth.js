b412394681037a5162beaf7a6c57faa4d5c614b7
Patrick <patrick@hiho.co.nz>
2019-08-01 14:52:10 +1200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -31,4 +31,32 @@ router.post("/submit", cors(), async (req, response) => {
 	}
 });
 
+router.post("/submit/winner", cors(), async (req, response) => {
+	dotenv.config();
+	const listId = req.body.list;
+	try {
+		res = await axios({
+			method: "POST",
+			url: `https://api.createsend.com/api/v3.2/subscribers/${listId}.json`,
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				Authorization: "Basic " + process.env.API_KEY
+			},
+			data: {
+				EmailAddress: req.body.email,
+				Name: req.body.name,
+				CustomFields: [{Key: "address", Value: req.body.address}, {Key: "Phone", Value: req.body.phone}],
+				Resubscribe: true,
+				RestartSubscriptionBasedAutoresponders: true,
+				ConsentToTrack: "Yes"
+			}
+		});
+
+		response.send("Successfully posted!");
+	} catch (err) {
+		response.send("Error: " + err);
+	}
+});
+
 module.exports = router;
